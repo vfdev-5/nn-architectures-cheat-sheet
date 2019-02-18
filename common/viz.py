9dede61eb65fbb84cd909d9a6cc41af3f9cff605
vfdev-5 <vfdev.5@gmail.com>
2019-02-18 11:39:34 +0100

    Updated resnet cheat-sheet

---
+++ common/viz.py
@@ -0,0 +1,28 @@
+# TensorFlow Graph visualizer code
+import random
+from IPython.display import clear_output, Image, display, HTML
+
+def show_graph(graph_def, max_const_size=32):
+    """Visualize TensorFlow graph."""
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    strip_def = graph_def
+    code = """
+        <script src="//cdnjs.cloudflare.com/ajax/libs/polymer/0.3.3/platform.js"></script>
+        <script>
+          function load() {{
+            document.getElementById("{id}").pbtxt = {data};
+          }}
+        </script>
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+          <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph_'+str(random.random()))
+
+    iframe = """
+        <iframe seamless style="width:1200px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
+    
+    
